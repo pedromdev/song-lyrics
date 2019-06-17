09ff8639f49b203cc5c7ecd642a79ce983dd7c04
Pedro Alves <pedromarcelodesaalves@gmail.com>
2019-06-17 01:31:28 -0300

    Added: addLyric mutation

--- src/server/__tests__/graphql/MutationType.js
+++ src/server/__tests__/graphql/MutationType.js
@@ -38,8 +38,6 @@ describe('GraphQL MutationType', () => {
         }
       `);
 
-      console.log(JSON.stringify(Song, null, 2));
-
       expect(Song).toHaveBeenCalledTimes(1);
       expect(Song).toHaveBeenCalledWith({name, artist, youtubeID});
       expect(save).toHaveBeenCalledTimes(1);
@@ -47,6 +45,105 @@ describe('GraphQL MutationType', () => {
       expect(song.name).toEqual(name);
       expect(song.artist).toEqual(artist);
       expect(song.youtubeID).toEqual(youtubeID);
+    });
+
+  });
+
+  describe('addLyric mutation', () => {
+
+    it('should add a lyric to a song', async () => {
+      const _id = new ObjectID();
+      const _song_id = new ObjectID();
+      const language = 'en';
+      const text = 'Hello my little world';
+      const startAt = 2;
+      const endAt = 6;
+
+      const {save} = Lyric.prototype;
+
+      Song.findById.mockResolvedValue({
+        _id: _song_id
+      });
+      save.mockResolvedValueOnce({
+        _id,
+        _song_id,
+        language,
+        text,
+        startAt,
+        endAt
+      });
+
+      const {data: {lyric}} = await graphql(Schema, `
+        mutation {
+          lyric: addLyric(
+            songId: "${_song_id.toHexString()}"
+            language: "${language}"
+            text: "${text}"
+            startAt: ${startAt}
+            endAt: ${endAt}
+          ) {
+            _id
+            language
+            text
+            startAt
+            endAt
+            song {
+              _id
+            }
+          }
+        }
+      `);
+
+      expect(Song.findById).toHaveBeenCalledTimes(2);
+      expect(Lyric).toHaveBeenCalledTimes(1);
+      expect(Lyric).toHaveBeenCalledWith({
+        _song_id,
+        language,
+        text,
+        startAt,
+        endAt
+      });
+      expect(save).toHaveBeenCalledTimes(1);
+      expect(save).toHaveBeenCalledWith();
+
+      expect(lyric._id).toEqual(_id.toHexString());
+      expect(lyric.language).toEqual(language);
+      expect(lyric.text).toEqual(text);
+      expect(lyric.startAt).toEqual(startAt);
+      expect(lyric.endAt).toEqual(endAt);
+      expect(lyric.song).not.toBeUndefined();
+      expect(lyric.song._id).toEqual(_song_id.toHexString());
+    });
+
+    it('should throw an error when a song not exist', async () => {
+      const _song_id = new ObjectID();
+
+      const {save} = Lyric.prototype;
+
+      Song.findById.mockResolvedValueOnce(null);
+
+      const {errors, data: {lyric}} = await graphql(Schema, `
+        mutation {
+          lyric: addLyric(
+            songId: "${_song_id.toHexString()}"
+            language: "en"
+            text: "Hello my little world"
+            startAt: 2
+            endAt: 6
+          ) {
+            _id
+          }
+        }
+      `);
+
+      expect(Song.findById).toHaveBeenCalledTimes(1);
+      expect(Song.findById).toHaveBeenCalledWith(_song_id.toHexString());
+      expect(Lyric).toHaveBeenCalledTimes(0);
+      expect(save).toHaveBeenCalledTimes(0);
+
+      expect(lyric).toBeNull();
+      expect(errors.length).toEqual(1);
+      expect(errors[0].message).toEqual('Song not found');
     });
 
   });
--- src/server/graphql/MutationType.js
+++ src/server/graphql/MutationType.js
@@ -7,6 +7,7 @@ const MutationType = new GraphQLObjectType({
   name: 'MutationType',
   fields: () => {
     const SongType = require('./types/SongType');
+    const LyricType = require('./types/LyricType');
 
     return {
       addSong: {
@@ -20,6 +21,31 @@ const MutationType = new GraphQLObjectType({
           const song = new Song({name, artist, youtubeID});
 
           return song.save();
+        }
+      },
+      addLyric: {
+        type: LyricType,
+        args: {
+          songId: {type: new GraphQLNonNull(GraphQLString)},
+          language: {type: new GraphQLNonNull(GraphQLString)},
+          text: {type: new GraphQLNonNull(GraphQLString)},
+          startAt: {type: new GraphQLNonNull(GraphQLInt)},
+          endAt: {type: new GraphQLNonNull(GraphQLInt)},
+        },
+        async resolve(parentValue, {songId, language, text, startAt, endAt}) {
+          const song = await Song.findById(songId);
+
+          if (song === null) throw new Error('Song not found');
+
+          const lyric = new Lyric({
+            _song_id: song._id,
+            language,
+            text,
+            startAt,
+            endAt
+          });
+
+          return await lyric.save();
         }
       }
     };
