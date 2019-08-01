21edb7016ca583b3179e27b9d9b7be8b474f277a
Pedro Alves <pedromarcelodesaalves@gmail.com>
2019-07-31 22:47:20 -0300

    Updated: rearranging tests

--- src/server/__tests__/graphql/types/LyricType.js
+++ src/server/__tests__/graphql/types/LyricType.js
@@ -8,176 +8,283 @@ const Schema = require('../../../graphql/Schema');
 jest.mock('../../../models/Song');
 jest.mock('../../../models/Lyric');
 
-describe('GraphQL SongType', () => {
+describe('GraphQL Lyric type', () => {
 
   const _song_id = new ObjectID();
   const _id = new ObjectID();
 
-  it('should list the song lyrics', async () => {
-    const query = `
-      {
-        song(id: "${_song_id.toHexString()}") {
-          _id
+  describe('Query', () => {
+
+    it('should list the song lyrics', async () => {
+      const query = `
+        {
+          song(id: "${_song_id.toHexString()}") {
+            _id
+            lyrics {
+              _id
+              language
+              text
+              startAt
+              endAt
+            }
+          }
+        }
+      `;
+      Song.findById.mockResolvedValueOnce({
+        _id: _song_id
+      });
+      Lyric.find.mockResolvedValueOnce([
+        {
+          _id,
+          language: 'en',
+          text: 'Hello my little world',
+          startAt: 2,
+          endAt: 6
+        }
+      ]);
+
+      const {data: {song: {lyrics}}} = await graphql(Schema, query);
+
+      expect(Song.findById).toHaveBeenCalledTimes(1);
+      expect(Song.findById).toHaveBeenCalledWith(_song_id.toHexString());
+      expect(Lyric.find).toHaveBeenCalledTimes(1);
+      expect(Lyric.find).toHaveBeenCalledWith({_song_id});
+
+      expect(lyrics).toBeInstanceOf(Array);
+      expect(lyrics.length).toEqual(1);
+      expect(lyrics[0]._id).toEqual(_id.toHexString());
+      expect(lyrics[0].language).toEqual('en');
+      expect(lyrics[0].text).toEqual('Hello my little world');
+      expect(lyrics[0].startAt).toEqual(2);
+      expect(lyrics[0].endAt).toEqual(6);
+    });
+
+    it('should filter the song lyrics', async () => {
+      const query = `
+        {
+          song(id: "${_song_id.toHexString()}") {
+            _id
+            lyrics(language: "en", text: "Hello my little world") {
+              _id
+            }
+          }
+        }
+      `;
+      Song.findById.mockResolvedValueOnce({
+        _id: _song_id
+      });
+      Lyric.find.mockResolvedValueOnce([
+        {
+          _id,
+          language: 'en',
+          text: 'Hello my little world',
+          startAt: 2,
+          endAt: 6
+        }
+      ]);
+
+      await graphql(Schema, query);
+
+      expect(Song.findById).toHaveBeenCalledTimes(1);
+      expect(Song.findById).toHaveBeenCalledWith(_song_id.toHexString());
+      expect(Lyric.find).toHaveBeenCalledTimes(1);
+      expect(Lyric.find.mock.calls[0].length).toEqual(1);
+      expect(Lyric.find.mock.calls[0][0]._song_id.toHexString()).toEqual(_song_id.toHexString());
+      expect(Lyric.find.mock.calls[0][0].language).toEqual('en');
+      expect(Lyric.find.mock.calls[0][0].text).toBeInstanceOf(RegExp);
+    });
+
+    it('should retrive the song from the lyric', async () => {
+      const query = `
+        {
+          song(id: "${_song_id.toHexString()}") {
+            lyrics {
+              song {
+                _id
+              }
+            }
+          }
+        }
+      `;
+      Song.findById.mockResolvedValue({
+        _id: _song_id
+      });
+      Lyric.find.mockResolvedValueOnce([
+        {
+          _id,
+          _song_id
+        }
+      ]);
+
+      const {data: {song: {lyrics: [lyric]}}} = await graphql(Schema, query);
+
+      expect(Song.findById).toHaveBeenCalledTimes(2);
+      expect(Song.findById).toHaveBeenCalledWith(_song_id.toHexString());
+
+      expect(lyric.song._id).toEqual(_song_id.toHexString());
+    });
+
+    it('should list the lyrics', async () => {
+      const query = `
+        {
           lyrics {
             _id
             language
             text
             startAt
             endAt
-          }
-        }
-      }
-    `;
-    Song.findById.mockResolvedValueOnce({
-      _id: _song_id
-    });
-    Lyric.find.mockResolvedValueOnce([
-      {
-        _id,
-        language: 'en',
-        text: 'Hello my little world',
-        startAt: 2,
-        endAt: 6
-      }
-    ]);
-
-    const {data: {song: {lyrics}}} = await graphql(Schema, query);
-
-    expect(Song.findById).toHaveBeenCalledTimes(1);
-    expect(Song.findById).toHaveBeenCalledWith(_song_id.toHexString());
-    expect(Lyric.find).toHaveBeenCalledTimes(1);
-    expect(Lyric.find).toHaveBeenCalledWith({_song_id});
-
-    expect(lyrics).toBeInstanceOf(Array);
-    expect(lyrics.length).toEqual(1);
-    expect(lyrics[0]._id).toEqual(_id.toHexString());
-    expect(lyrics[0].language).toEqual('en');
-    expect(lyrics[0].text).toEqual('Hello my little world');
-    expect(lyrics[0].startAt).toEqual(2);
-    expect(lyrics[0].endAt).toEqual(6);
-  });
-
-  it('should filter the song lyrics', async () => {
-    const query = `
-      {
-        song(id: "${_song_id.toHexString()}") {
-          _id
-          lyrics(language: "en", text: "Hello my little world") {
-            _id
-          }
-        }
-      }
-    `;
-    Song.findById.mockResolvedValueOnce({
-      _id: _song_id
-    });
-    Lyric.find.mockResolvedValueOnce([
-      {
-        _id,
-        language: 'en',
-        text: 'Hello my little world',
-        startAt: 2,
-        endAt: 6
-      }
-    ]);
-
-    await graphql(Schema, query);
-
-    expect(Song.findById).toHaveBeenCalledTimes(1);
-    expect(Song.findById).toHaveBeenCalledWith(_song_id.toHexString());
-    expect(Lyric.find).toHaveBeenCalledTimes(1);
-    expect(Lyric.find.mock.calls[0].length).toEqual(1);
-    expect(Lyric.find.mock.calls[0][0]._song_id.toHexString()).toEqual(_song_id.toHexString());
-    expect(Lyric.find.mock.calls[0][0].language).toEqual('en');
-    expect(Lyric.find.mock.calls[0][0].text).toBeInstanceOf(RegExp);
-  });
-
-  it('should retrive the song from the lyric', async () => {
-    const query = `
-      {
-        song(id: "${_song_id.toHexString()}") {
-          lyrics {
             song {
               _id
             }
           }
         }
-      }
-    `;
-    Song.findById.mockResolvedValue({
-      _id: _song_id
+      `;
+      Song.findById.mockResolvedValue({
+        _id: _song_id
+      });
+      Lyric.find.mockResolvedValueOnce([
+        {
+          _id: new ObjectID(),
+          language: 'en',
+          text: 'Hello my little world',
+          startAt: 2,
+          endAt: 6,
+          _song_id
+        },
+        {
+          _id: new ObjectID(),
+          language: 'en',
+          text: 'I\'ll love anyone',
+          startAt: 2,
+          endAt: 6,
+          _song_id
+        }
+      ]);
+
+      const {data: {lyrics}} = await graphql(Schema, query);
+
+      expect(Song.findById).toHaveBeenCalledTimes(2);
+      expect(Song.findById).toHaveBeenCalledWith(_song_id);
+      expect(Lyric.find).toHaveBeenCalledTimes(1);
+      expect(Lyric.find).toHaveBeenCalledWith({});
+
+      expect(lyrics).toBeInstanceOf(Array);
+      expect(lyrics.length).toEqual(2);
+
+      lyrics.forEach(lyric => {
+        expect(lyric._id).not.toBeUndefined();
+        expect(lyric.language).toEqual('en');
+        expect(lyric.text).not.toBeUndefined();
+        expect(lyric.startAt).not.toBeUndefined();
+        expect(lyric.endAt).not.toBeUndefined();
+        expect(lyric.song).not.toBeUndefined();
+        expect(lyric.song._id).toEqual(_song_id.toHexString());
+      })
     });
-    Lyric.find.mockResolvedValueOnce([
-      {
-        _id,
-        _song_id
-      }
-    ]);
 
-    const {data: {song: {lyrics: [lyric]}}} = await graphql(Schema, query);
-
-    expect(Song.findById).toHaveBeenCalledTimes(2);
-    expect(Song.findById).toHaveBeenCalledWith(_song_id.toHexString());
-
-    expect(lyric.song._id).toEqual(_song_id.toHexString());
   });
 
-  it('should list the lyrics', async () => {
-    const query = `
-      {
-        lyrics {
-          _id
-          language
-          text
-          startAt
+  describe('Mutation', () => {
+
+    describe('addLyric', () => {
+
+      it('should add a lyric to a song', async () => {
+        const _id = new ObjectID();
+        const _song_id = new ObjectID();
+        const language = 'en';
+        const text = 'Hello my little world';
+        const startAt = 2;
+        const endAt = 6;
+
+        const {save} = Lyric.prototype;
+
+        Song.findById.mockResolvedValue({
+          _id: _song_id
+        });
+        save.mockResolvedValueOnce({
+          _id,
+          _song_id,
+          language,
+          text,
+          startAt,
           endAt
-          song {
-            _id
+        });
+
+        const {data: {lyric}} = await graphql(Schema, `
+          mutation {
+            lyric: addLyric(
+              songId: "${_song_id.toHexString()}"
+              language: "${language}"
+              text: "${text}"
+              startAt: ${startAt}
+              endAt: ${endAt}
+            ) {
+              _id
+              language
+              text
+              startAt
+              endAt
+              song {
+                _id
+              }
+            }
           }
-        }
-      }
-    `;
-    Song.findById.mockResolvedValue({
-      _id: _song_id
+        `);
+
+        expect(Song.findById).toHaveBeenCalledTimes(2);
+        expect(Lyric).toHaveBeenCalledTimes(1);
+        expect(Lyric).toHaveBeenCalledWith({
+          _song_id,
+          language,
+          text,
+          startAt,
+          endAt
+        });
+        expect(save).toHaveBeenCalledTimes(1);
+        expect(save).toHaveBeenCalledWith();
+
+        expect(lyric._id).toEqual(_id.toHexString());
+        expect(lyric.language).toEqual(language);
+        expect(lyric.text).toEqual(text);
+        expect(lyric.startAt).toEqual(startAt);
+        expect(lyric.endAt).toEqual(endAt);
+        expect(lyric.song).not.toBeUndefined();
+        expect(lyric.song._id).toEqual(_song_id.toHexString());
+      });
+
+      it('should throw an error when a song not exist', async () => {
+        const _song_id = new ObjectID();
+
+        const {save} = Lyric.prototype;
+
+        Song.findById.mockResolvedValueOnce(null);
+
+        const {errors, data: {lyric}} = await graphql(Schema, `
+          mutation {
+            lyric: addLyric(
+              songId: "${_song_id.toHexString()}"
+              language: "en"
+              text: "Hello my little world"
+              startAt: 2
+              endAt: 6
+            ) {
+              _id
+            }
+          }
+        `);
+
+        expect(Song.findById).toHaveBeenCalledTimes(1);
+        expect(Song.findById).toHaveBeenCalledWith(_song_id.toHexString());
+        expect(Lyric).toHaveBeenCalledTimes(0);
+        expect(save).toHaveBeenCalledTimes(0);
+
+        expect(lyric).toBeNull();
+        expect(errors.length).toEqual(1);
+        expect(errors[0].message).toEqual('Song not found');
+      });
+
     });
-    Lyric.find.mockResolvedValueOnce([
-      {
-        _id: new ObjectID(),
-        language: 'en',
-        text: 'Hello my little world',
-        startAt: 2,
-        endAt: 6,
-        _song_id
-      },
-      {
-        _id: new ObjectID(),
-        language: 'en',
-        text: 'I\'ll love anyone',
-        startAt: 2,
-        endAt: 6,
-        _song_id
-      }
-    ]);
 
-    const {data: {lyrics}} = await graphql(Schema, query);
-
-    expect(Song.findById).toHaveBeenCalledTimes(2);
-    expect(Song.findById).toHaveBeenCalledWith(_song_id);
-    expect(Lyric.find).toHaveBeenCalledTimes(1);
-    expect(Lyric.find).toHaveBeenCalledWith({});
-
-    expect(lyrics).toBeInstanceOf(Array);
-    expect(lyrics.length).toEqual(2);
-
-    lyrics.forEach(lyric => {
-      expect(lyric._id).not.toBeUndefined();
-      expect(lyric.language).toEqual('en');
-      expect(lyric.text).not.toBeUndefined();
-      expect(lyric.startAt).not.toBeUndefined();
-      expect(lyric.endAt).not.toBeUndefined();
-      expect(lyric.song).not.toBeUndefined();
-      expect(lyric.song._id).toEqual(_song_id.toHexString());
-    })
   });
 
 });
--- src/server/__tests__/graphql/types/SongType.js
+++ src/server/__tests__/graphql/types/SongType.js
@@ -6,95 +6,142 @@ const Schema = require('../../../graphql/Schema');
 
 jest.mock('../../../models/Song');
 
-describe('GraphQL SongType', () => {
+describe('GraphQL Song type', () => {
 
-  it('should query a song', async () => {
-    const _id = new ObjectID();
-    const query = `
-      {
-        song(id: "${_id.toHexString()}") {
-          _id
-          name
-          artist
-          youtubeID
+  describe('Query', () => {
+
+    it('should query a song', async () => {
+      const _id = new ObjectID();
+      const query = `
+        {
+          song(id: "${_id.toHexString()}") {
+            _id
+            name
+            artist
+            youtubeID
+          }
         }
-      }
-    `;
-    Song.findById.mockResolvedValueOnce({
-      _id,
-      name: 'Name B',
-      artist: 'Artist B',
-      youtubeID: 'youtubeIDB',
-    });
-
-    const {data: {song}} = await graphql(Schema, query);
-
-    expect(Song.findById).toHaveBeenCalledTimes(1);
-    expect(Song.findById).toHaveBeenCalledWith(_id.toHexString());
-
-    expect(song._id).toEqual(_id.toHexString());
-    expect(song.name).toEqual('Name B');
-    expect(song.artist).toEqual('Artist B');
-    expect(song.youtubeID).toEqual('youtubeIDB');
-  });
-
-  it('should query a list of songs', async () => {
-    const query = `
-      {
-        songs {
-          _id
-          name
-          artist
-        }
-      }
-    `;
-    Song.find.mockResolvedValueOnce([
-      {
-        _id: new ObjectID(),
-        name: 'Name A',
-        artist: 'Artist A',
-        youtubeID: 'youtubeIDA'
-      },
-      {
-        _id: new ObjectID(),
+      `;
+      Song.findById.mockResolvedValueOnce({
+        _id,
         name: 'Name B',
-        artist: 'Artst B',
-        youtubeID: 'youtubeIDB'
-      }
-    ]);
+        artist: 'Artist B',
+        youtubeID: 'youtubeIDB',
+      });
 
-    const {data: {songs}} = await graphql(Schema, query);
+      const {data: {song}} = await graphql(Schema, query);
 
-    expect(Song.find).toHaveBeenCalledTimes(1);
-    expect(Song.find).toHaveBeenCalledWith({});
+      expect(Song.findById).toHaveBeenCalledTimes(1);
+      expect(Song.findById).toHaveBeenCalledWith(_id.toHexString());
 
-    expect(songs).toBeInstanceOf(Array);
-    expect(songs.length).toEqual(2);
-
-    songs.forEach((song) => {
-      expect(song._id).not.toBeUndefined();
-      expect(song.name).not.toBeUndefined();
-      expect(song.artist).not.toBeUndefined();
-      expect(song.youtubeID).toBeUndefined();
+      expect(song._id).toEqual(_id.toHexString());
+      expect(song.name).toEqual('Name B');
+      expect(song.artist).toEqual('Artist B');
+      expect(song.youtubeID).toEqual('youtubeIDB');
     });
+
+    it('should query a list of songs', async () => {
+      const query = `
+        {
+          songs {
+            _id
+            name
+            artist
+          }
+        }
+      `;
+      Song.find.mockResolvedValueOnce([
+        {
+          _id: new ObjectID(),
+          name: 'Name A',
+          artist: 'Artist A',
+          youtubeID: 'youtubeIDA'
+        },
+        {
+          _id: new ObjectID(),
+          name: 'Name B',
+          artist: 'Artst B',
+          youtubeID: 'youtubeIDB'
+        }
+      ]);
+
+      const {data: {songs}} = await graphql(Schema, query);
+
+      expect(Song.find).toHaveBeenCalledTimes(1);
+      expect(Song.find).toHaveBeenCalledWith({});
+
+      expect(songs).toBeInstanceOf(Array);
+      expect(songs.length).toEqual(2);
+
+      songs.forEach((song) => {
+        expect(song._id).not.toBeUndefined();
+        expect(song.name).not.toBeUndefined();
+        expect(song.artist).not.toBeUndefined();
+        expect(song.youtubeID).toBeUndefined();
+      });
+    });
+
+    it('should filter a list of songs', async () => {
+      const query = `
+        {
+          songs(name: "Name", artist: "Artist", youtubeID: "yID") {
+            _id
+          }
+        }
+      `;
+
+      await graphql(Schema, query);
+
+      expect(Song.find).toHaveBeenCalledTimes(1);
+      expect(Song.find.mock.calls[0].length).toEqual(1);
+      expect(Song.find.mock.calls[0][0].name).toBeInstanceOf(RegExp);
+      expect(Song.find.mock.calls[0][0].artist).toBeInstanceOf(RegExp);
+      expect(Song.find.mock.calls[0][0].youtubeID).toEqual('yID');
+    });
+
   });
 
-  it('should filter a list of songs', async () => {
-    const query = `
-      {
-        songs(name: "Name", artist: "Artist", youtubeID: "yID") {
-          _id
-        }
-      }
-    `;
+  describe('Mutation', () => {
 
-    await graphql(Schema, query);
+    describe('addSong', () => {
 
-    expect(Song.find).toHaveBeenCalledTimes(1);
-    expect(Song.find.mock.calls[0].length).toEqual(1);
-    expect(Song.find.mock.calls[0][0].name).toBeInstanceOf(RegExp);
-    expect(Song.find.mock.calls[0][0].artist).toBeInstanceOf(RegExp);
-    expect(Song.find.mock.calls[0][0].youtubeID).toEqual('yID');
+      it('should add a song', async () => {
+        const _id = new ObjectID();
+        const name = 'Name';
+        const artist = 'Artist';
+        const youtubeID = 'youtubeID';
+
+        const {save} = Song.prototype;
+
+        save.mockResolvedValueOnce({
+          _id,
+          name,
+          artist,
+          youtubeID
+        });
+
+        const {data: {song}} = await graphql(Schema, `
+          mutation {
+            song: addSong(name: "${name}", artist: "${artist}", youtubeID: "${youtubeID}") {
+              _id
+              name
+              artist
+              youtubeID
+            }
+          }
+        `);
+
+        expect(Song).toHaveBeenCalledTimes(1);
+        expect(Song).toHaveBeenCalledWith({name, artist, youtubeID});
+        expect(save).toHaveBeenCalledTimes(1);
+        expect(song._id).toEqual(_id.toHexString());
+        expect(song.name).toEqual(name);
+        expect(song.artist).toEqual(artist);
+        expect(song.youtubeID).toEqual(youtubeID);
+      });
+
+    });
+
   });
 
 });
